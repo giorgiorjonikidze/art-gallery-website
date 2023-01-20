23429eda5e1f9df2ef0a23dd517d206083ea4430
giorgi orjonikidze <gioorjo123@gmail.com>
2023-01-20 16:36:55 +0400

    make landingPage resposnive for desktops

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -17,7 +17,12 @@ const Landing = () => {
   };
   return (
     <div>
-      <section className="mb-[120px] relative">
+      <section className="mb-[120px] relative xl:flex">
+        <div className="w-[540px] h-[800px] bg-black hidden md:hidden xl:block "></div>
+        <div className="mixx  top-[116px] left-[256px] font-black text-[96px] w-[520px] hidden md:hidden xl:block absolute z-40">
+          <p className="text-white font-black text-[96px]  ">MODERN</p>
+          <p className="text-white font-black text-[96px] absolute ">ART GALLERY</p>
+        </div>
         <picture>
           <source
             media="(min-width: 1400px)"
@@ -34,11 +39,11 @@ const Landing = () => {
           />
         </picture>
 
-        <div className="flex flex-col px-[16px] md:w-[340px] md:absolute md:top-[145px] md:right-[40px]">
-          <h1 className="font-black text-[60px] mb-[32px] leading-[55px] md:mb-[48px] ">
+        <div className="flex flex-col px-[16px] md:w-[340px] md:absolute md:top-[145px] md:right-[40px] xl:absolute xl:top-[200px] xl:right-[100px]">
+          <h1 className="font-black text-[60px] mb-[32px] leading-[55px] md:mb-[48px] xl:hidden">
             MODERN ART GALLERY
           </h1>
-          <p className="font-[outfit] text-[18px] mb-[32px]  md:mb-[48px]">
+          <p className="font-[outfit] text-[18px] mb-[32px]  md:mb-[48px] xl:text-[22px]">
             The arts in the collection of the Modern Art Gallery all started
             from a spark of inspiration. Will these pieces inspire you? Visit us
             and find out.
@@ -56,7 +61,7 @@ const Landing = () => {
           </button>
         </div>
       </section>
-      <section className="flex flex-col px-[16px] md:flex-row-reverse md:px-[40px] md:justify-between">
+      <section className="flex flex-col px-[16px] md:flex-row-reverse md:px-[40px] md:justify-between xl:px-[165px]">
         <picture>
           <source
             media="(min-width: 1400px)"
@@ -86,7 +91,7 @@ const Landing = () => {
           </p>
         </div>
       </section>
-      <section className="mb-[120px] md:px-[40px] md:flex">
+      <section className="mb-[120px] md:px-[40px] md:flex xl:px-[165px]">
         <picture>
           <source
             media="(min-width: 1400px)"
@@ -106,7 +111,7 @@ const Landing = () => {
             className="px-[16px] mb-[16px] md:px-0"
           />
         </picture>
-        <div className="px-[16px]">
+        <div className="px-[16px] xl:pr-0">
           <picture>
             <source
               media="(min-width: 1400px)"
@@ -126,20 +131,20 @@ const Landing = () => {
               className="mb-[16px]"
             />
           </picture>
-          <div className="bg-[#000] h-[293px] text-[#fff] px-[24px] py-[47px] box-border md:px-0 md:w-[280px] md:pl-[30px] md:h-[351px]">
-            <h3 className="font-black text-[50px] leading-[45px] mb-[24px]">
+          <div className="bg-[#000] h-[293px] text-[#fff] px-[24px] py-[47px] box-border md:px-0 md:w-[280px] md:pl-[30px] md:h-[351px] xl:w-[448px] xl:h-[388px] ">
+            <h3 className="font-black text-[50px] leading-[45px] mb-[24px] xl:text-[60px] xl:leading-[60px]">
               COME & BE INSPIRED
             </h3>
-            <p className="font-[outfit] text-[18px] font-light">
+            <p className="font-[outfit] text-[18px] font-light xl:w-[350px]">
               We’re excited to welcome you to our gallery and see how our
               collections influence you.
             </p>
           </div>
         </div>
       </section>
-      <section className="bg-[#000] text-white box-border px-[32px] py-[48px] flex flex-col md:flex-row md:px-[40px] md:justify-between">
+      <section className="bg-[#000] text-white box-border px-[32px] py-[48px] flex flex-col md:flex-row md:px-[40px] md:justify-between xl:px-[165px] xl:pt-[80px]">
         <img src={logoWhite} className="mb-[38px] h-[40px] w-[110px]" />
-        <p className="font-[outfit] mb-[38px] leading-7 font-light md:w-[281px] md:mb-0">
+        <p className="font-[outfit] mb-[38px] leading-7 font-light md:w-[281px] md:mb-0 xl:w-[430px]">
           The Modern Art Gallery is free to all visitors and open seven days a
           week from 8am to 9pm. Find us at 99 King Street, Newport, USA.
         </p>
